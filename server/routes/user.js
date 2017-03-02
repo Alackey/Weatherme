ae46f13dfb7dbfa78b4a11d378da1c200135546e
Anthony Lackey <alackey96@gmail.com>
2017-03-01 18:46:06 -0800

    Nodemon working with babel

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res) => {
-  res.send('respond with a resource');
+  res.send('users route');
 });
 
 export default router;
